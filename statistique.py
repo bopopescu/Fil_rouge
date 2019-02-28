8b11acb01bb3f02fc66f77849c49204a92312bda
oliviermayousse <olivier.mayousse@gmail.com>
2019-02-28 11:03:06 +0100

    stats nombre de partie par jour

--- statistique.py
+++ statistique.py
@@ -3,30 +3,17 @@ import json
 
 partie_gen = []
 stats_jour = {}
+for fiche in os.listdir('./partie'):
+    with open('./partie/' + fiche, 'r') as fichier:
+        stats = json.loads(fichier.read())
+        print(stats)
+        partie_gen.append(stats)
 
 
-def main(partie_gen):
-    for fiche in os.listdir('./partie'):
-        with open('./partie/' + fiche, 'r') as fichier:
-            stats = json.loads(fichier.read())
-            print(stats)
-            partie_gen.append(stats)
-    return partie_gen
-
-
-def stat_partie_jour():
-    cpt = 0
-    for item in partie_gen:
-        if item["Start time"][:8] == item["Start time"][:8]:
-            cpt += 1
-    return cpt
-
-
-
-
-main(partie_gen)
-nombre = stat_partie_jour()
-print(nombre)
-
-
-
+for elements in partie_gen:
+    date = elements["Start time"][:8]
+    if date in stats_jour.keys():
+        stats_jour[date] += 1
+    else:
+        stats_jour[date] = 1
+print(stats_jour)
